90f79d17b41f4d3d24b040fd24448fd13c211cac
Stoyan Ivanov <98014562+stiv03@users.noreply.github.com>
2024-12-02 17:30:22 +0200

    Update Main.java

--- zad/src/Main.java
+++ zad/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         BankDepositorManager manager = new BankDepositorManager();
@@ -6,14 +8,23 @@ public class Main {
 
         BankDepositor depositor1 = new BankDepositor("1234567890", "Ivan Ivanov", 1000.00, depositDate);
         BankDepositor depositor2 = new BankDepositor("0987654321", "Petar Petrov", 2000.00, depositDate);
+        BankDepositor depositor3 = new BankDepositor("5324524355", "Georgi Marinov", 500.00, depositDate);
 
         manager.addDepositor(depositor1);
         manager.addDepositor(depositor2);
+        manager.addDepositor(depositor3);
 
         manager.sortDepositorsByAmount();
-        manager.displayDepositors();
+
+        depositor2.setDepositAmount(1000);
+
 
         manager.saveToFile("depositors.txt");
-        manager.loadFromFile("depositors.txt");
+
+
+        List<BankDepositor> list = manager.loadFromFile("depositors.txt");
+        if (list!= null) {
+            list.forEach(System.out::println);
+        }
     }
 }
